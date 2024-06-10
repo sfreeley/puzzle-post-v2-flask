ed3f05e2f7d038bddcf0dd061d2b8136d56148c7
sfreeley <65191331+sfreeley@users.noreply.github.com>
2024-06-09 22:38:32 -0400

    add edit profile form, GET, POST functionality

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app import db
 import sqlalchemy as sa
 from app.models import User
@@ -36,4 +36,9 @@ class RegistrationForm(FlaskForm):
         ))
         if user is not None:
             raise ValidationError('Email already exists')
+        
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
     
